dc288551ce950fb18e94db8c1d71f5808b541791
User <y55mun33@gmail.com>
2023-04-09 20:39:52 +0900

    사용자 컴포넌트 사용

--- ch02/ch02_3/src/App.tsx
+++ ch02/ch02_3/src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <ul>
+      <li>
+        <a href="http://www.google.com">
+          <p>go to Google</p>
         </a>
-      </header>
-    </div>
-  );
+      </li>
+    </ul>
+  )
 }
-
-export default App;
