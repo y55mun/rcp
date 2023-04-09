5bd53c0d48c9127ca5ea62eb529d6f21910fee48
User <y55mun33@gmail.com>
2023-04-09 20:43:02 +0900

    클래스 컴포넌트로 구현

--- ch02/ch02_3/src/App.tsx
+++ ch02/ch02_3/src/App.tsx
@@ -1,11 +1,15 @@
-export default function App() {
-  return (
-    <ul>
-      <li>
-        <a href="http://www.google.com">
-          <p>go to Google</p>
-        </a>
-      </li>
-    </ul>
-  )
+import {Component} from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <a href="http://www.google.com">
+            <p>go to Google</p>
+          </a>
+        </li>
+      </ul>
+    )
+  }
 }
