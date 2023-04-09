62d0da9875facc72c43c4e2c8a986addc9dc1be5
User <y55mun33@gmail.com>
2023-04-09 20:48:40 +0900

    isLoading 값에 따라 분기

--- ch02/ch02_3/src/App.tsx
+++ ch02/ch02_3/src/App.tsx
@@ -3,19 +3,18 @@ import {Component} from 'react'
 export default class App extends Component {
   render() {
     const isLoading = true
-    const children = (
-      <li>
-        <a href="http://www.google.com">
-          <p>go to Google</p>
-        </a>
-      </li>
+    const children = isLoading ? (
+      <p>loading...</p>
+    ) : (
+      <ul>
+        <li>
+          <a href="http://www.google.com">
+            <p>go to Google</p>
+          </a>
+        </li>
+      </ul>
     )
 
-    return (
-      <div>
-        {isLoading && <p>loading...</p>}
-        {!isLoading && <ul>{children}</ul>}
-      </div>
-    )
+    return <div>{children}</div>
   }
 }
