3bc308bbbeed4df1583439117f794bc7359196db
User <y55mun33@gmail.com>
2023-04-10 18:34:27 +0900

    .

--- ch02/ch02_4/src/App.tsx
+++ ch02/ch02_4/src/App.tsx
@@ -1,4 +1,4 @@
 export default function App() {
-  const texts = ['hello', 'world'].map((text, index) => <p key={index}>{text}</p>)
-  return <div>{texts}</div>
+  const texts = ['hello', 'world'].map((text, index) => <p key={index} children={text} />)
+  return <div children={texts} />
 }
