73de91b5e10442d273b5e1b1c42e6c36b3766fdc
User <y55mun33@gmail.com>
2023-04-14 23:29:10 +0900

    DragDrop 구현중

--- ch02/ch02_5/src/pages/DragDrop.tsx
+++ ch02/ch02_5/src/pages/DragDrop.tsx
@@ -1,3 +1,14 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+import type {DragEvent} from 'react'
+
+export default function DragDrop() {
+  const onDragStart = (e: DragEvent<HTMLElement>) =>
+    console.log('onDragStart', e.dataTransfer)
+  const onDragEnd = (e: DragEvent<HTMLElement>) =>
+    console.log('onDragEnd', e.dataTransfer)
+
+  const onDragOver = (e: DragEvent) => e.preventDefault()
+  const onDrop = (e: DragEvent) => {
+    e.preventDefault()
+    console.log('onDrop', e.dataTransfer)
+  }
 }
