564b47b59d1ef259b4aeacc4c7bb99aefce38f12
User <y55mun33@gmail.com>
2023-05-01 20:54:39 +0900

    .

--- ch03/ch03_3/src/components/Icon.tsx
+++ ch03/ch03_3/src/components/Icon.tsx
@@ -3,15 +3,11 @@ import type {FC, DetailedHTMLProps, HTMLAttributes} from 'react'
 type ReactSpanProps = DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>
 
 export type IconProps = ReactSpanProps & {
-  name: String
+  name: string
 }
 
-// prettire-ignore
+//prettier-ignore
 export const Icon: FC<IconProps> = ({name, className: _className, ...props}) => {
   const className = ['material-icons', _className].join(' ')
-  return (
-    <span {...props} className={className}>
-      {name}
-    </span>
-  )
+  return <span {...props} className={className}>{name}</span>
 }
