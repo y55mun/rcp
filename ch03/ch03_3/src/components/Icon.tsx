6a158cb5c097576787bf11df64c3a2f2c9c8c1ac
User <y55mun33@gmail.com>
2023-05-02 20:18:56 +0900

    props에 잔여 연산자 구문 적용

--- ch03/ch03_3/src/components/Icon.tsx
+++ ch03/ch03_3/src/components/Icon.tsx
@@ -1,13 +1,14 @@
-import type {FC, DetailedHTMLProps, HTMLAttributes} from 'react'
+import type {FC, CSSProperties} from 'react'
 
-type ReactSpanProps = DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>
-
-export type IconProps = ReactSpanProps & {
+export type IconProps = {
   name: string
+  style?: CSSProperties
 }
 
-//prettier-ignore
-export const Icon: FC<IconProps> = ({name, className: _className, ...props}) => {
-  const className = ['material-icons', _className].join(' ')
-  return <span {...props} className={className}>{name}</span>
+export const Icon: FC<IconProps> = ({name, ...props}) => {
+  return (
+    <span {...props} className="material-icons">
+      {name}
+    </span>
+  )
 }
