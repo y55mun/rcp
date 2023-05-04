ad2a336e3934ffcbc850e4c2897fa75a34bb0e3d
User <y55mun33@gmail.com>
2023-05-05 00:00:59 +0900

    텍스트 관련 컴포넌트 사용

--- ch03/ch03_3/src/pages/TextsTest.tsx
+++ ch03/ch03_3/src/pages/TextsTest.tsx
@@ -11,6 +11,14 @@ export default function TextsTest() {
       <div>
         <Title className="text-blue-600">{D.randomTitleText()}</Title>
         <Subtitle className="text-blue-400">{D.randomSentence()}</Subtitle>
+        <p className="text-xl italic text-center text-gray-900 text-bold">
+          {D.randomName()}
+        </p>
+        <Paragraph numberOfLines={5}>{paragraphs}</Paragraph>
+        <Summary className="text-center text-gray-500">{summery}</Summary>
+        <p className="text-center text-pink-400">
+          {D.randomDayMonthYear()} ({D.randomRelativeDate()})
+        </p>
       </div>
     </div>
   )
