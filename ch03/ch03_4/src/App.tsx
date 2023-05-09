2d92b3ad8839af2ba95677407e94da5c153113ec
User <y55mun33@gmail.com>
2023-05-09 23:14:31 +0900

    init

--- ch03/ch03_4/src/App.tsx
+++ ch03/ch03_4/src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DivTest from './pages/DivTest'
+import ViewportTest from './pages/ViewportTest'
+import HeightTest from './pages/HeightTest'
+import PaddingTest from './pages/PaddingTest'
+import MarginTest from './pages/MarginTest'
+import ImageTest from './pages/ImageTest'
+import BackgroundImageTest from './pages/BackgroundImageTest'
+import DisplayTest from './pages/DisplayTest'
+import DisplayNoneTest from './pages/DisplayNoneTest'
+import AvatarTest from './pages/AvatarTest'
+import PositionTest from './pages/PositionTest'
+import OverlayTest from './pages/OverlayTest'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <OverlayTest />
+      <PositionTest />
+      <DisplayNoneTest />
+      <DisplayTest />
+      <AvatarTest />
+      <BackgroundImageTest />
+      <ImageTest />
+      <MarginTest />
+      <PaddingTest />
+      <HeightTest />
+      <ViewportTest />
+      <DivTest />
+    </main>
+  )
 }
-
-export default App;
