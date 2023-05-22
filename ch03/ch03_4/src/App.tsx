07a805f7ed6f4d0db1a6ca1e5f255ef8dcf5bfc4
User <y55mun33@gmail.com>
2023-05-22 21:58:29 +0900

    import 추가

--- ch03/ch03_4/src/App.tsx
+++ ch03/ch03_4/src/App.tsx
@@ -1,31 +1,7 @@
-import DivTest from './pages/DivTest'
-import ViewportTest from './pages/ViewportTest'
-import HeightTest from './pages/HeightTest'
-import PaddingTest from './pages/PaddingTest'
-import MarginTest from './pages/MarginTest'
-import ImageTest from './pages/ImageTest'
-import BackgroundImageTest from './pages/BackgroundImageTest'
-import DisplayTest from './pages/DisplayTest'
-import DisplayNoneTest from './pages/DisplayNoneTest'
-import AvatarTest from './pages/AvatarTest'
-import PositionTest from './pages/PositionTest'
-import OverlayTest from './pages/OverlayTest'
-
-export default function App() {
-  return (
-    <main>
-      <OverlayTest />
-      <PositionTest />
-      <DisplayNoneTest />
-      <DisplayTest />
-      <AvatarTest />
-      <BackgroundImageTest />
-      <ImageTest />
-      <MarginTest />
-      <PaddingTest />
-      <HeightTest />
-      <ViewportTest />
-      <DivTest />
-    </main>
-  )
-}
+import DirectionTest from './pages/DirectionTest'
+import WrapTest from './pages/WrapTest'
+import MinMaxTest from './pages/MinMaxTest'
+import JustifyCenterTest from './pages/JustifyCenterTest'
+import AlignTest from './pages/AlignTest'
+import UserContainer from './pages/UserContainer'
+import CardContainer from './pages/CardContainer'
